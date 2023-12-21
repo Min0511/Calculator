0014a614981d7f7ded90e090bf76469b0b33f52d
ymlee0511 <0511ymlee@gmail.com>
2023-12-20 20:38:03 -0800

    Add all the function of buttons. It needs to be updated for the case /0

---
+++ script.js
@@ -0,0 +1,91 @@
+
+const nums = document.querySelectorAll(".num");
+const numsArray = Array.from(nums);
+const screen = document.querySelector(".screen");
+const reset = document.querySelector(".reset");
+const del = document.querySelector(".delete");
+const equal = document.querySelector(".equal")
+const symbols = document.querySelectorAll(".sym");
+const symsArray = Array.from(symbols);
+let storedNumber = null;
+let storedSym = null
+
+numsArray.forEach((num)=>{
+    num.addEventListener("click",()=>{
+        if(screen.textContent === "0") {
+            screen.textContent = num.textContent;
+        }else{
+            screen.textContent += num.textContent;
+        }
+        symsArray.forEach((sym)=>{
+            if(sym.getAttribute("class").includes("pressedButton")){
+                sym.classList.remove('pressedButton');
+                screen.textContent = num.textContent;
+            };
+
+        });
+    });
+});
+
+symsArray.forEach((sym)=>{
+    sym.addEventListener("click",()=>{
+
+        sym.classList.add("pressedButton");
+        let getNumber = Number(screen.textContent);
+        
+        console.log(storedSym)
+        console.log(storedNumber)
+        if(storedNumber !== null){
+            switch(storedSym){
+                case "+":
+                    storedNumber += getNumber;
+                    break;
+                case "-":
+                    storedNumber -= getNumber;
+                    break;
+                case "*":
+                    storedNumber *= getNumber;
+                    break;
+                    case "/":
+                    storedNumber /= getNumber;
+                    break;
+                }
+        }else {
+            storedNumber = getNumber;
+        }
+        storedSym = sym.textContent
+        screen.textContent = storedNumber;
+    });
+
+});
+
+equal.addEventListener("click",() => {
+    let getNumber = Number(screen.textContent);
+    switch(storedSym){
+        case "+":
+            storedNumber += getNumber;
+            break;
+        case "-":
+            storedNumber -= getNumber;
+            break;
+        case "*":
+            storedNumber *= getNumber;
+            break;
+            case "/":
+            storedNumber /= getNumber;
+            break;
+        }
+    storedSym = null;
+    screen.textContent = storedNumber;
+});
+
+reset.addEventListener("click",() => {
+    storedNumber = null;
+    screen.textContent = "0";
+})
+del.addEventListener("click", () => {
+    screen.textContent = screen.textContent.slice(0,screen.textContent.length -1)
+    
+})
+
+console.log(screen.textContent)
